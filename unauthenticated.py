b84ec96093009941a0d32ca08777b4424268f245
mattiamarilli <mattiamarilli@gmail.com>
2023-11-14 09:40:33 +0100

    fix name

--- unauthenticated.py
+++ unauthenticated.py
@@ -9,6 +9,7 @@ secret_key_r, public_key_r = hpke.generate_key_pair()  # receiver keys, pre-gene
 message = b"MAMBO JUMBO!"
 encap, ciphertext = hpke.seal(public_key_r, info, aad, message)
 
+
 # ============= Receiver =============
 
 plaintext = hpke.open(encap, secret_key_r, info, aad, ciphertext)
