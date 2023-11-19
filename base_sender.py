b296976c619287728f3b8e155c5f963f0c768ac2
mattiamarilli <mattiamarilli@gmail.com>
2023-11-19 15:48:39 +0100

    fix

--- base_sender.py
+++ base_sender.py
@@ -27,11 +27,6 @@ aad = b""  # shared metadata, message-level
 message = b"MAMBO JUMBO!"
 encap, ciphertext = hpke.seal(public_key_r, info, aad, message)
 
-datatosend = {
-    'encap': encap.decode().replace("'", '"'),
-    'ciphertext':  ciphertext.decode().replace("'", '"'),
-}
+data_to_send = encap + b',' + ciphertext
 
-jsontosend = json.dumps(datatosend)
-
-sock.sendto(jsontosend, (UDP_IP, 5002))
+sock.sendto(data_to_send, (UDP_IP, 5002))
